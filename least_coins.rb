98ebc4a8b4ecf9133a6f84b9cec75a194a4dfd66
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-26 18:18:53 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,25 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+  q=0 
+  d=0 
+  n=0 
+  p=0 
+  until cents < 25
+    cents-=25
+    q += 1
+  end
+  until cents < 10 
+    cents-=10
+    d+=1
+  end
+  until cents < 5 
+    cents -= 5 
+    n+= 1
+  end
+  p=cents 
+  coin_count = {:quarters => q, :dimes => d, :nickels => n, :pennies => p}
 end
+
+puts least_coins(67)
+#Code your answer here!
